24fe414d7f35545457b3fcf95f97b99f6cd5159a
Patrick Cyubahiro <ampatrickcyubahiro@gmail.com>
2022-04-17 15:19:42 +0300

    Create Interface_in_Java.java

---
+++ Interface_in_Java.java
@@ -0,0 +1,20 @@
+// create an interface
+interface Language {
+  void getName(String name);
+}
+
+// class implements interface
+class ProgrammingLanguage implements Language {
+
+  // implementation of abstract method
+  public void getName(String name) {
+    System.out.println("One of my favorite programming languages is: " + name);
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    ProgrammingLanguage language = new ProgrammingLanguage();
+    language.getName("Java");
+  }
+}
